709c9f961ddeee9f03f0d3d29cba01414cc901dc
jtdv01 <jtdv01@gmail.com>
2019-05-16 21:18:03 +1000

    Db parser

--- src/user_hierarchy/main.py
+++ src/user_hierarchy/main.py
@@ -1,14 +1,20 @@
 #! /usr/bin/python3
 from parser import arg_parser, parse_roles_and_users
-from user import User
-from role import Role
+import pdb
 
 def main():
     args = arg_parser()
     roles, users = parse_roles_and_users(args.input_file)
 
+    db = {}
 
+    for r in roles:
+        parent = r.parent
+        # Initialise a new key
+        if parent not in db.keys():
+            db[parent] = []
 
+    print(db)
 
 if __name__ == '__main__':
     main()
--- src/user_hierarchy/role.py
+++ src/user_hierarchy/role.py
@@ -1,6 +1,11 @@
 class Role():
 
-    def __init__(self, id: int, name: str, parent: 'Role'):
+    def __init__(self, id: int, name: str, parent: int):
         self.id = id
         self.name = name
         self.parent = parent
+        self.children_roles = []
+
+    def add_child_role(self, child_role: 'Role'):
+        self.children_roles.append(child_role)
+
