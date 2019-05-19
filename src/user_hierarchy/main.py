6c2725cac7914a667cff0e4f67637328c417259e
jtdv01 <jtdv01@gmail.com>
2019-05-19 16:13:17 +1000

    Added logger

--- src/user_hierarchy/main.py
+++ src/user_hierarchy/main.py
@@ -1,11 +1,22 @@
 #! /usr/bin/python3
 from parser import arg_parser, parse_roles_and_users
 from db import init_db
+import logging
+from datetime import datetime
+
+def setup_logger():
+    FORMAT = "%(asctime)-15s %(levelname)s-%(message)s"
+    today = datetime.now().strftime("%Y-%m-%d")
+    logfile = f"logs/{today}.log"
+    logging.basicConfig(format=FORMAT, filename=logfile, filemode='w+', level=logging.INFO)
 
 def main():
+    setup_logger()
     args = arg_parser()
     roles, users = parse_roles_and_users(args.roles_filepath, args.users_filepath)
     db = init_db(roles, users)
+
+    logging.info(db)
 
     wait_for_user_input = True
     while wait_for_user_input:
@@ -13,7 +24,7 @@ def main():
         try:
             query_user_id = int(user_input)
             user = db['users'][query_user_id]
-            print(f"User {user.id} has a Role ID {user.role.id}")
+            logging.info(f"User {user.id} has a Role ID {user.role.id}")
         except ValueError as e:
             print("Invalid user input")
         except KeyError as e:
